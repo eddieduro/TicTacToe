f92fef2dd2c9d0802d5afbb49d8855e562958ec7
alissa paluska and eddie duro <alissa.paluska@gmail.com>
2016-01-27 11:56:16 -0800

    lunch time, save progress, no updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,85 @@
-Player = function(symbol){
-  this.symbol = symbol;
-  this.scores = [];
-};
-
-Player.prototype.getScore = function(value1, value2, value3){
-  this.scores.push(value1, value2, value3);
-  return this.scores;
-};
-
-
-winningCombos = [
+var winningCombos = [
   [1,2,3],[4,5,6],[7,8,9],[3,6,9],[2,5,8],
   [1,4,7],[1,5,9],[3,5,7]
-]
+];
+var scores = [];
+
+Player = function(symbol){
+  this.symbol = symbol;
+};
+Score = function(val1, val2, val3, val4, val5){
+  this.val1 = val1;
+  this.val2 = val2;
+  this.val3 = val3;
+  this.val4 = val4;
+  this.val5 = val5;
+};
+
+
+score = {
+  var scores = []
+  getScore: function(val1, val2, val3, val4, val5){
+    this.val1 = val1;
+    this.val2 = val2;
+    this.val3 = val3;
+    this.val4 = val4;
+    this.val5 = val5;
+  },
+  pushScore: function(){
+    scores.push(this.val1, this.val2, this.val3, this.val4, this.val5);
+  },
+  evaluate: function(){
+    var error = "You ruined the game";
+    var winning = "You won!"
+
+    console.log(joinCombo);
+
+    if((joinScore.substring(0,3) !== joinCombo && (joinScore.length === 5))
+    || (joinScore.substring(1,4) !== joinCombo && (joinScore.length === 5))
+    || (joinScore.substring(2,5) !== joinCombo && (joinScore.length === 5))) {
+      return joinScore;
+    } else if ((joinScore.substring(0,3) !== joinCombo)
+    || (joinScore.substring(1,4) !== joinCombo)
+    || (joinScore.substring(2,5) !== joinCombo)) {
+      return joinScore;
+    } else if (joinScore.substring(0,3) === joinCombo) {
+        return winning;
+    } else {
+      return error;
+    }
+  }
+
+}
+var joinCombo;
+
+Score.prototype.getScore = function(){
+  scores.push(this.val1, this.val2, this.val3, this.val4, this.val5);
+  for(var i = 0; i < winningCombos.length; i++){
+    // console.log(scores.slice(0,3));
+    joinCombo = winningCombos[i].join("");
+
+  };
+};
+  var joinScore = scores.join("");
+  console.log(joinScore);
+// evaluate = function(){
+//   var error = "You ruined the game";
+//   var winning = "You won!"
+//
+//   console.log(joinCombo);
+//
+//   if((joinScore.substring(0,3) !== joinCombo && (joinScore.length === 5))
+//   || (joinScore.substring(1,4) !== joinCombo && (joinScore.length === 5))
+//   || (joinScore.substring(2,5) !== joinCombo && (joinScore.length === 5))) {
+//     return joinScore;
+//   } else if ((joinScore.substring(0,3) !== joinCombo)
+//   || (joinScore.substring(1,4) !== joinCombo)
+//   || (joinScore.substring(2,5) !== joinCombo)) {
+//     return joinScore;
+//   } else if (joinScore.substring(0,3) === joinCombo) {
+//       return winning;
+//   } else {
+//     return error;
+//   }
+// }
+// evaluate(testScore.getScore())
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,26 @@
 describe('Player', function(){
+  var testPlayer = new Player("X");
   it('will create a Player object', function(){
-    var testPlayer = new Player("X");
     expect(testPlayer.symbol).to.equal("X");
   });
+});
 
-  it('will return array of values selected', function(){
-    var testPlayer = new Player("X");
-    expect(testPlayer.getScore(1 , 2 , 3)).to.eql([1, 2, 3]);
+describe('Score', function(){
+  var testScore = new Score(1,2,3);
+  var testScore2 = new Score(2,5,7);
+
+  it('will evaluate scores and return 3 in a row', function(){
+    expect(testScore.getScore()).to.equal("You Won!");
+  });
+  it('will return string of values selected and compare it to the winning combos', function(){
+    expect(testScore2.getScore()).to.equal("257");
   });
 });
+
+
+
+
+  // it('will not return 3 in a row', function(){
+  //   var testEvaluation = [3,2,8];
+  //   expect(testEvaluation).to.equal("Loser");
+  // });
