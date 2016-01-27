65dd55cf838ae8ead9803995e986970e21e05ec4
alissa paluska and eddie duro <e.jamesduro@gmail.com>
2016-01-27 08:58:22 -0800

    second spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 Player = function(symbol){
   this.symbol = symbol;
-  this.score = [];
+  this.scores = [];
 };
+
+Player.prototype.getScore = function(value1, value2, value3){
+  this.scores.push(value1, value2, value3);
+  return this.scores;
+};
+
+
+winningCombos = [
+  [1,2,3],[4,5,6],[7,8,9],[3,6,9],[2,5,8],
+  [1,4,7],[1,5,9],[3,5,7]
+]
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,11 @@
-// describe('Board', function(){
-//   it('will create a board with 9 spaces')
-// });
-
 describe('Player', function(){
   it('will create a Player object', function(){
     var testPlayer = new Player("X");
     expect(testPlayer.symbol).to.equal("X");
+  });
+
+  it('will return array of values selected', function(){
+    var testPlayer = new Player("X");
+    expect(testPlayer.getScore(1 , 2 , 3)).to.eql([1, 2, 3]);
   });
 });
