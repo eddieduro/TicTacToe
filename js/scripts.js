06e00abb4bd41af25b29d568c7b874fd7e1c5ea6
alissa paluska and eddie duro <e.jamesduro@gmail.com>
2016-01-27 15:24:53 -0800

    revert just incase

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ var winningCombos = [
   [1,4,7],[1,5,9],[3,5,7]
 ];
 var scores = [];
+var joinScore;
+var joinCombo;
+var playerTurn;
 
 Player = function(symbol){
   this.symbol = symbol;
 };
+
 Score = function(val1, val2, val3, val4, val5){
   this.val1 = val1;
   this.val2 = val2;
@@ -15,71 +19,97 @@ Score = function(val1, val2, val3, val4, val5){
   this.val5 = val5;
 };
 
-
-score = {
-  var scores = []
-  getScore: function(val1, val2, val3, val4, val5){
-    this.val1 = val1;
-    this.val2 = val2;
-    this.val3 = val3;
-    this.val4 = val4;
-    this.val5 = val5;
-  },
-  pushScore: function(){
-    scores.push(this.val1, this.val2, this.val3, this.val4, this.val5);
-  },
-  evaluate: function(){
-    var error = "You ruined the game";
-    var winning = "You won!"
-
-    console.log(joinCombo);
-
-    if((joinScore.substring(0,3) !== joinCombo && (joinScore.length === 5))
-    || (joinScore.substring(1,4) !== joinCombo && (joinScore.length === 5))
-    || (joinScore.substring(2,5) !== joinCombo && (joinScore.length === 5))) {
-      return joinScore;
-    } else if ((joinScore.substring(0,3) !== joinCombo)
-    || (joinScore.substring(1,4) !== joinCombo)
-    || (joinScore.substring(2,5) !== joinCombo)) {
-      return joinScore;
-    } else if (joinScore.substring(0,3) === joinCombo) {
-        return winning;
-    } else {
-      return error;
-    }
-  }
-
-}
-var joinCombo;
-
-Score.prototype.getScore = function(){
+Score.prototype.joinScore = function() {
   scores.push(this.val1, this.val2, this.val3, this.val4, this.val5);
-  for(var i = 0; i < winningCombos.length; i++){
-    // console.log(scores.slice(0,3));
-    joinCombo = winningCombos[i].join("");
+  joinScore = scores.join("");
+  return joinScore;
+};
 
+joinComboFunction = function(){
+  for(var i = 0; i < winningCombos.length; i++){
+    joinCombo = winningCombos[i].join("");
+    return joinCombo;
   };
 };
-  var joinScore = scores.join("");
+
+
+Score.prototype.evaluate = function(){
+  var error = "You ruined the game";
+  var winning = "You won!"
   console.log(joinScore);
-// evaluate = function(){
-//   var error = "You ruined the game";
-//   var winning = "You won!"
+  joinComboFunction();
+
+  if((joinScore.substring(0,3) !== joinCombo && (joinScore.length === 5))
+  || (joinScore.substring(1,4) !== joinCombo && (joinScore.length === 5))
+  || (joinScore.substring(2,5) !== joinCombo && (joinScore.length === 5))) {
+    return joinScore;
+  } else if ((joinScore.substring(0,3) !== joinCombo)
+  || (joinScore.substring(1,4) !== joinCombo)
+  || (joinScore.substring(2,5) !== joinCombo)) {
+    return joinScore;
+  } else if (joinScore.substring(0,3) === joinCombo) {
+    return winning;
+  } else {
+    return error;
+  };
+};
+
+$(document).ready(function(){
+  var arr = [];
+
+// playerChoice = function(){
+//   $('td#one').click(function(){
+//     var Player1 = new Score();
+//     var pushVal = (Player1.val1 = 1);
+//     arr.push(pushVal);
+//     console.log(arr)
+//   });
 //
-//   console.log(joinCombo);
-//
-//   if((joinScore.substring(0,3) !== joinCombo && (joinScore.length === 5))
-//   || (joinScore.substring(1,4) !== joinCombo && (joinScore.length === 5))
-//   || (joinScore.substring(2,5) !== joinCombo && (joinScore.length === 5))) {
-//     return joinScore;
-//   } else if ((joinScore.substring(0,3) !== joinCombo)
-//   || (joinScore.substring(1,4) !== joinCombo)
-//   || (joinScore.substring(2,5) !== joinCombo)) {
-//     return joinScore;
-//   } else if (joinScore.substring(0,3) === joinCombo) {
-//       return winning;
-//   } else {
-//     return error;
-//   }
+//   $('td#two').click(function(){
+//     var Player1 = new Score();
+//     var pushVal = (Player1.val2 = 2);
+//     arr.push(pushVal);
+//     console.log(arr);
+//   });
+//   $('td#three').click(function(){
+//     var Player1 = new Score();
+//     Player1.val3 = 3;
+//     console.log(Player1);
+//   });
+//   $('td#four').click(function(){
+//     var Player1 = new Score();
+//     Player1.val4 = 4;
+//     console.log(Player1);
+//   });
+//   $('td#five').click(function(){
+//     var Player1 = new Score();
+//     Player1.val5 = 5;
+//     console.log(Player1);
+//   });
+//   $('td#six').click(function(){
+//     var Player1 = new Score();
+//     Player1.val6 = 6;
+//     console.log(Player1);
+//   });
+//   $('td#seven').click(function(){
+//     var Player1 = new Score();
+//     Player1.val7 = 7;
+//     console.log(Player1);
+//   });
+//   $('td#eight').click(function(){
+//     var Player1 = new Score();
+//     Player1.val8 = 8;
+//     console.log(Player1);
+//   });
+//   $('td#nine').click(function(){
+//     var Player1 = new Score();
+//     Player1.val9 = 9;
+//     console.log(Player1);
+//   });
 // }
-// evaluate(testScore.getScore())
+// playerChoice();
+});
+var newPlayer = new Score(1,2,3);
+var y = newPlayer.joinScore();
+console.log(y);
+joinComboFunction();
--- spec/specs.js
+++ spec/specs.js
@@ -5,16 +5,17 @@ describe('Player', function(){
   });
 });
 
-describe('Score', function(){
-  var testScore = new Score(1,2,3);
-  var testScore2 = new Score(2,5,7);
+describe('evaluate', function(){
+    var testScore = new Score(1,2,3);
+    it('will evaluate scores and return 3 in a row', function(){
+      expect(testScore.evaluate()).to.equal("You won!");
+    });
+  // var testScore2 = new Score(2,5,7);
 
-  it('will evaluate scores and return 3 in a row', function(){
-    expect(testScore.getScore()).to.equal("You Won!");
-  });
-  it('will return string of values selected and compare it to the winning combos', function(){
-    expect(testScore2.getScore()).to.equal("257");
-  });
+
+  // it('will return string of values selected and compare it to the winning combos', function(){
+  //   expect(testScore2.getScore()).to.equal("257");
+  // });
 });
 
 
@@ -24,3 +25,14 @@ describe('Score', function(){
   //   var testEvaluation = [3,2,8];
   //   expect(testEvaluation).to.equal("Loser");
   // });
+
+
+takeTurn
+  - activated by click listener
+  - add that spot to the players score
+  - run evaluate function to see if theres a winner
+  - (if no winner: switch the playerTurn variable)
+  - (if winner: )
+
+* board with buttons of shared classes;
+class removed when clicked on
